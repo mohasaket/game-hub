d9feb446c8a368447c148d27943d2c07808a05f1
Moha <m.saket1990@gmail.com>
2023-09-01 09:20:26 +0330

    resable api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,26 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-
-export default axios.create({
+//declare as local constant
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "9b61590c4d4e4be081f8b74daa6290c3",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+  static getAll: any;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+export default APIClient;
